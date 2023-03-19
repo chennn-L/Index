03b75be5a85c81772dfc23ff3eb156b141cffb84
chennn-L <1306400316@qq.com>
2023-03-19 16:53:00 +0800

    smote算法实现

--- smote.py
+++ smote.py
@@ -1,55 +1,64 @@
+import random
+import xlrd
+import xlwt  # 负责写excel
+from sklearn.neighbors import NearestNeighbors
 import numpy as np
-import random     #生成随机数
-import matplotlib.pyplot as plt   #画图
-# import pandas as pd
-# mount = pd.read_excel('ma2.xlsx')
-# print(mount)
+import scipy.io as scio
+class Smote:
+    def __init__(self,samples,N=10,k=5):
+        self.n_samples,self.n_attrs=samples.shape
+        self.N=N
+        self.k=k
+        # sanples是距离决定因子
+        self.samples=samples
+        # smote算法已合成的样本个数
+        self.newindex=0
+       # self.synthetic=np.zeros((self.n_samples*N,self.n_attrs))
 
-import scipy.io as scio    #引用mat文件
-mat_path = './GSE2034_ma2.mat'
-load_mat = scio.loadmat(mat_path)
-# mat_path = './test.mat'
-# print(load_mat)
-# print (load_mat['ma2'],load_mat['ma2'].shape)
+    def over_sampling(self):
+        N=int(self.N/100)
+        self.synthetic = np.zeros((self.n_samples * N, self.n_attrs))
+        neighbors=NearestNeighbors(n_neighbors=self.k).fit(self.samples)
+        print ('neighbors',neighbors)
+        for i in range(len(self.samples)):
+            nnarray=neighbors.kneighbors(self.samples[i].reshape(1,-1),return_distance=False)[0]
+            #print nnarray
+            self._populate(N,i,nnarray)
+        return self.synthetic
 
 
+    # for each minority class samples,choose N of the k nearest neighbors and generate N synthetic samples.
+    def _populate(self,N,i,nnarray):
+        for j in range(N):
+            nn=random.randint(0,self.k-1)
+            dif=self.samples[nnarray[nn]]-self.samples[i]
+            gap=random.random()
+            self.synthetic[self.newindex]=self.samples[i]+gap*dif
+            self.newindex+=1
 
-class SMOTE(object):
-    def __init__(self,sample,k=2,gen_num=3):
-        self.sample = sample      
-        self.sample_num,self.feature_len = self.sample.shape
-        self.k = min(k,self.sample_num-1)                
-        self.gen_num = gen_num    
-        self.syn_data = np.zeros((self.gen_num,self.feature_len))  
-        self.k_neighbor = np.zeros((self.sample_num,self.k),dtype=int)  
-
-    def get_neighbor_point(self):
-        for index,single_signal in enumerate(self.sample):
-            Euclidean_distance = np.array([np.sum(np.square(single_signal-i)) for i in self.sample])
-            Euclidean_distance_index = Euclidean_distance.argsort()
-            self.k_neighbor[index] = Euclidean_distance_index[1:self.k+1]
-
-    def get_syn_data(self):
-        self.get_neighbor_point()
-        for i in range(self.gen_num):
-            key = random.randint(0,self.sample_num-1)
-            K_neighbor_point = self.k_neighbor[key][random.randint(0,self.k-1)]
-            gap = self.sample[K_neighbor_point] - self.sample[key]
-            self.syn_data[i] = self.sample[key] + random.uniform(0,1)*gap
-        return self.syn_data
-
-if __name__ == '__main__':
-    #随机生成原始数据
-    # data=np.random.uniform(0,1,size=[20,2])
-    data = load_mat['ma2']
-    #生成对象k=5 gen_num=20
-    Syntheic_sample = SMOTE(data,5,20)
-    #生成数据
-    new_data = Syntheic_sample.get_syn_data()
-	#绘制原始数据
-    for i in data:
-        plt.scatter(i[0],i[1],c='b')
-	#绘制生成数据
-    for i in new_data:
-        plt.scatter(i[0],i[1],c='y')
-    plt.show()
+# 把xlsx文件转化成矩阵
+def excel2matrix(path):
+    data = xlrd.open_workbook(path)
+    table = data.sheets()[0]
+    nrows = table.nrows  # 行数
+    ncols = table.ncols  # 列数
+    datamatrix = np.zeros((nrows, ncols))
+    for i in range(ncols):
+        cols = table.col_values(i)
+        datamatrix[:, i] = cols
+    return datamatrix
+  
+pathX = 'pos_data.xlsx'  
+x = excel2matrix(pathX)
+a=x
+s=Smote(a,N=100)
+print (s.over_sampling())
+print (s.over_sampling().shape)
+  
+filename =xlwt.Workbook() #创建工作簿
+sheet1 = filename.add_sheet(u'sheet1',cell_overwrite_ok=True) #创建sheet
+[h,l]=s.over_sampling().shape #h为行数，l为列数
+for i in range (h):
+    for j in range (l):
+        sheet1.write(i,j,s.over_sampling[i,j])
+filename.save('pos_data.xls')
---
+++ smote1.py
@@ -0,0 +1,117 @@
+import random
+import xlrd
+import xlwt
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+ 
+class Smote(object):
+    
+    def __init__(self, N=50, k=5, r=2):
+        # 初始化self.N, self.k, self.r, self.newindex
+        self.N = N
+        self.k = k
+        # self.r是距离决定因子
+        self.r = r
+        # self.newindex用于记录SMOTE算法已合成的样本个数
+        self.newindex = 0
+        
+    # 构建训练函数
+    def fit(self, samples):
+        # 初始化self.samples, self.T, self.numattrs
+        self.samples = samples
+        # self.T是少数类样本个数，self.numattrs是少数类样本的特征个数
+        self.T, self.numattrs = self.samples.shape
+
+        # 查看N%是否小于100%
+        if(self.N < 100):
+            # 如果是，随机抽取N*T/100个样本，作为新的少数类样本
+            np.random.shuffle(self.samples)
+            self.T = int(self.N*self.T/100)
+            self.samples = self.samples[0:self.T,:]
+            # N%变成100%
+            self.N = 100
+
+        # 查看从T是否不大于近邻数k
+        if(self.T <= self.k):
+            # 若是，k更新为T-1
+            self.k = self.T - 1
+
+        # 令N是100的倍数
+        N = int(self.N/100)
+        # 创建保存合成样本的数组
+        self.synthetic = np.zeros((self.T * N, self.numattrs))
+
+        # 调用并设置k近邻函数
+        neighbors = NearestNeighbors(n_neighbors=self.k+1, 
+                                     algorithm='ball_tree', 
+                                     p=self.r).fit(self.samples)
+
+        # 对所有输入样本做循环
+        for i in range(len(self.samples)):
+            # 调用kneighbors方法搜索k近邻
+            nnarray = neighbors.kneighbors(self.samples[i].reshape((1,-1)),
+                                           return_distance=False)[0][1:]
+
+            # 把N,i,nnarray输入样本合成函数self.__populate
+            self.__populate(N, i, nnarray)
+
+        # 最后返回合成样本self.synthetic
+        return self.synthetic
+    
+    # 构建合成样本函数
+    def __populate(self, N, i, nnarray):
+        # 按照倍数N做循环
+        for j in range(N):
+            # attrs用于保存合成样本的特征
+            attrs = []
+            # 随机抽取1～k之间的一个整数，即选择k近邻中的一个样本用于合成数据
+            nn = random.randint(0, self.k-1)
+            
+            # 计算差值
+            diff = self.samples[nnarray[nn]] - self.samples[i]
+            # 随机生成一个0～1之间的数
+            gap = random.uniform(0,1)
+            # 合成的新样本放入数组self.synthetic
+            self.synthetic[self.newindex] = self.samples[i] + gap*diff
+
+            # self.newindex加1， 表示已合成的样本又多了1个
+            self.newindex += 1
+
+# samples = np.array([[3,1,2], [4,3,3], [1,3,4],
+#                     [3,3,2], [2,2,1], [1,4,3]])
+# smote = Smote(N=200)
+# synthetic_points = smote.fit(samples)
+# print(synthetic_points)
+
+# 把xlsx文件转化成矩阵
+def excel2matrix(path):
+    data = xlrd.open_workbook(path)
+    table = data.sheets()[0]
+    nrows = table.nrows  # 行数
+    ncols = table.ncols  # 列数
+    datamatrix = np.zeros((nrows, ncols))
+    for i in range(ncols):
+        cols = table.col_values(i)
+        datamatrix[:, i] = cols
+    return datamatrix
+
+pathX = 'pos_data.xlsx' 
+matrix = excel2matrix(pathX) #把xlsx文件转成数组
+# print(type(matrix))
+samples = matrix.transpose() #数组转置
+# print(samples.shape)
+smote = Smote(N=200)
+synthetic_points = smote.fit(samples)
+print(synthetic_points)
+# print(synthetic_points.shape)
+example = synthetic_points.transpose()
+
+workbook = xlwt.Workbook()
+sheet = workbook.add_sheet("Sheet")
+
+for i in range(len(example)):
+    for j in range(len(example[i])):
+        sheet.write(i, j, example[i][j])
+
+workbook.save("smote.xlsx")
+
